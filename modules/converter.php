cca4636349a3a92b82ce3bcf9e387bb331de8734
NgocSon15 <56545138+NgocSon15@users.noreply.github.com>
2020-04-29 22:55:25 +0700

    add new curency

--- modules/converter.php
+++ modules/converter.php
@@ -3,9 +3,23 @@
 		<div class="from">
 			<div id="from-list" class="list">
 				<select name="input-currency">
-					<option value="USD" data-id = "2">Đô-la Mỹ (USD)</option>
-					<option value="VND" data-id = "1" <?php if($currency1 == "VND") echo "selected" ?> >Việt Nam Đồng (VND)</option>
-					<option value="EUR" data-id = "3" <?php if($currency1 == "EUR") echo "selected" ?> >Euro (EUR)</option>
+					<option value="USD">Đô-la Mỹ (USD)</option>
+					<option value="VND" <?php if($currency1 == "VND") echo "selected" ?> >Việt Nam Đồng (VND)</option>
+					<option value="EUR" <?php if($currency1 == "EUR") echo "selected" ?> >Euro (EUR)</option>
+					<option value="CNY" <?php if($currency1 == "CNY") echo "selected" ?> >Đồng Yuan Trung Quốc (CNY)</option>
+					<option value="JPY" <?php if($currency1 == "JPY") echo "selected" ?> >Yên Nhật (JPY)</option>
+					<option value="GBP" <?php if($currency1 == "GBP") echo "selected" ?> >Bảng Anh (GBP)</option>
+					<option value="CAD" <?php if($currency1 == "CAD") echo "selected" ?> >Đô-la Canada (CAD)</option>
+					<option value="CHF" <?php if($currency1 == "CHF") echo "selected" ?> >Đồng Franc Thụy Sĩ (CHF)</option>
+					<option value="AUD" <?php if($currency1 == "AUD") echo "selected" ?> >Đô-la Australia (AUD)</option>
+					<option value="HKD" <?php if($currency1 == "HKD") echo "selected" ?> >Đô-la Hồng Kông (HKD)</option>
+					<option value="SGD" <?php if($currency1 == "SGD") echo "selected" ?> >Đô-la Singapore (SGD)</option>
+					<option value="MYR" <?php if($currency1 == "MYR") echo "selected" ?> >Đồng Ringgit Malaysia (MYR)</option>
+					<option value="IDR" <?php if($currency1 == "IDR") echo "selected" ?> >Đồng Rupia Indonesia (IDR)</option>
+					<option value="THB" <?php if($currency1 == "THB") echo "selected" ?> >Đồng Baht Thái Lan (THB)</option>
+					<option value="PHP" <?php if($currency1 == "PHP") echo "selected" ?> >Đồng Peso Philippines (PHP)</option>
+					<option value="MMK" <?php if($currency1 == "MMK") echo "selected" ?> >Đồng Kyat Myanmar (MMK)</option>
+					<option value="RUB" <?php if($currency1 == "RUB") echo "selected" ?> >Đồng Rúp Nga (RUB)</option>
 				</select>
 			</div>
 			<div>
@@ -18,9 +32,23 @@
 		<div class="to">
 			<div id="to-list" class="list">
 				<select name="output-currency">
-					<option value="VND" data-id = "1">Việt Nam Đồng (VND)</option>
-					<option value="USD" data-id = "2" <?php if($currency2 == "USD") echo "selected" ?> >Đô-la Mỹ (USD)</option>
-					<option value="EUR" data-id = "3" <?php if($currency2 == "EUR") echo "selected" ?> >Euro (EUR)</option>
+					<option value="VND">Việt Nam Đồng (VND)</option>
+					<option value="USD" <?php if($currency2 == "USD") echo "selected" ?> >Đô-la Mỹ (USD)</option>
+					<option value="EUR" <?php if($currency2 == "EUR") echo "selected" ?> >Euro (EUR)</option>
+					<option value="CNY" <?php if($currency2 == "CNY") echo "selected" ?> >Đồng Yuan Trung Quốc (CNY)</option>
+					<option value="JPY" <?php if($currency2 == "JPY") echo "selected" ?> >Yên Nhật (JPY)</option>
+					<option value="GBP" <?php if($currency2 == "GBP") echo "selected" ?> >Bảng Anh (GBP)</option>
+					<option value="CAD" <?php if($currency2 == "CAD") echo "selected" ?> >Đô-la Canada (CAD)</option>
+					<option value="CHF" <?php if($currency2 == "CHF") echo "selected" ?> >Đồng Franc Thụy Sĩ (CHF)</option>
+					<option value="AUD" <?php if($currency2 == "AUD") echo "selected" ?> >Đô-la Australia (AUD)</option>
+					<option value="HKD" <?php if($currency2 == "HKD") echo "selected" ?> >Đô-la Hồng Kông (HKD)</option>
+					<option value="SGD" <?php if($currency2 == "SGD") echo "selected" ?> >Đô-la Singapore (SGD)</option>
+					<option value="MYR" <?php if($currency2 == "MYR") echo "selected" ?> >Đồng Ringgit Malaysia (MYR)</option>
+					<option value="IDR" <?php if($currency2 == "IDR") echo "selected" ?> >Đồng Rupia Indonesia (IDR)</option>
+					<option value="THB" <?php if($currency2 == "THB") echo "selected" ?> >Đồng Baht Thái Lan (THB)</option>
+					<option value="PHP" <?php if($currency2 == "PHP") echo "selected" ?> >Đồng Peso Philippines (PHP)</option>
+					<option value="MMK" <?php if($currency2 == "MMK") echo "selected" ?> >Đồng Kyat Myanmar (MMK)</option>
+					<option value="RUB" <?php if($currency2 == "RUB") echo "selected" ?> >Đồng Rúp Nga (RUB)</option>
 				</select>
 			</div>
 			<div>
