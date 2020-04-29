9f02a6f4b0a6cf2ab881b5e37c6feb414c992cad
tienhgm <47660593+tienhgm@users.noreply.github.com>
2020-04-29 18:10:50 +0700

    hien thi bang

--- modules/showMoney.php
+++ modules/showMoney.php
@@ -1,16 +1,26 @@
 
 <div class="show_table">
 		<h1 class="h1">Bảng tỷ giá tiền tệ</h1>
-	<table border="1px">
-		<thead>
+		<table border="1px">
 			<tr>
+				<th>STT</th>
 				<th>Quốc Gia </th>
 				<th>Đồng</th>
 				<th>Giá đổi sang đồng VND</th>
 			</tr>	
-		</thead>
-		<tbody>
-		
-		</tbody>
+			<?php
+				include('lib/database.php');
+				$sqlSelect = "select * from ti_gia";
+				 $result = mysqli_query($conn,$sqlSelect) or die("failed");
+				if($result -> num_rows >0){
+					while ($row = $result -> fetch_assoc()) {
+						# code...
+						echo "<tr><td>".$row["countryid"] ."</td><td>". $row["country_name"]."</td><td>". $row["currency"]."</td><td>". $row["tigia"];
+					}
+				}
+					
+					
+			?>
+		 	
 	</table>
 </div>
