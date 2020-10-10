a3a004e37e4f09ccf80dfbd8edf227a4bf260936
NgocSon15 <56545138+NgocSon15@users.noreply.github.com>
2020-10-10 18:59:33 +0700

    update database

--- lib/database.php
+++ lib/database.php
@@ -1,8 +1,8 @@
 <?php
    $host   = "remotemysql.com";
-   $user   = "J20H6rdYuv";
-   $pass   = "73QPsApvZK";
-   $dbname = "J20H6rdYuv";
+   $user   = "D5np0Doa1R";
+   $pass   = "4Ht6uQ4wiP";
+   $dbname = "D5np0Doa1R";
 
    $conn = mysqli_connect($host, $user, $pass, $dbname) or die('không kết nối được');
    mysqli_select_db($conn, $dbname);
